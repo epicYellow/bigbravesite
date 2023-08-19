5085f264c864f24e8c91afedc8bc0c73b961c2a0
epicYellow <105212574+epicYellow@users.noreply.github.com>
2023-08-19 17:50:22 +0200

    Installed react, basic routing and pages set up

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Form from "./components/pages/Form/From";
+import Home from "./components/pages/Home/Home";
+import Result from "./components/pages/Result/Result";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/Form" element={Form} />
+        <Route path="/Result" element={Result} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
